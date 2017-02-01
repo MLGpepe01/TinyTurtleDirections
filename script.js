4714c092a5e2d4b284eab78e08a1e7cff1fe672d
David Sliva <sliva@citypolyhs.org>
2017-02-01 20:27:49 +0000

    asd

---
+++ script.js
@@ -0,0 +1,19 @@
+
+/* global forward */
+/* global TinyTurtle */
+/*global left*/
+TinyTurtle.apply(window);
+forward(40);
+right(90);
+forward(80);
+right(90); 
+forward(40)
+right(90);
+forward(80);
+right(90);
+forward(40);
+right(80);
+forward(40);
+right(22.5);
+forward(40);
+
